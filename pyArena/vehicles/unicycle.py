bf117ed84af3b2f7c525467976dc9f41eb5c302d
r-praveen-jain <jain1912@gmail.com>
2019-04-13 11:06:13 +0100

    [Remove] Unwanted files and folder Vehicles/unicycle

--- pyArena/vehicles/unicycle.py
+++
@@ -1,8 +0,0 @@
-# TODOC Summary: ...
-
-import numpy as np
-
-# IDEA Should that become a class?
-# Specify robot kinematics
-def unicycle(t, x, u):
-    return np.array([u[0]*np.cos(x[2]), u[0]*np.sin(x[2]), u[1], u[2]])
