fdb0f16ca5d70e7dcee56dede91700454abc9d65
gandradeng <gandrade.eng@gmail.com>
2020-03-27 17:42:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='pyarena',
+setup(name='pyArena',
       version='0.1',
       description='A python library of robotic algorithms',
       author='Praveen R. Jain and Romulo T. Rodrigues',
       license='GPLv3',
-      packages=find_namespace_packages(include=['pyarena.*']),
+      packages=find_namespace_packages(include=['pyArena.*']),
       zip_safe=False)
