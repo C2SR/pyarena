b5a0e1f442cc0e2691d79ad2cdb74146c0886e6c
Romulo <romulortr@gmail.com>
2020-04-08 17:54:43 +0200

    example with world

--- examples/unicycle.py
+++ examples/unicycle.py
@@ -1,25 +1,36 @@
 import numpy as np
 import time
 
-from pyArena.vehicles import unicycle
-from pyArena.sensors import landmark 
+from pyArena.vehicles.unicycle import Unicycle
+from pyArena.world.landmark_world import LandmarkWorld
+from pyArena.sensors.landmark_sensor import LandmarkSensor  
+from pyArena.plots.landmark_localization import LandmarkLocalization
 
-# Vehicle parametes
+np.random.seed(0)
+
+# Vehicle 
 x0 = np.array([0.0,0.0,0])
 kwargsUnicycle = {'x0': x0}
-vehicle = unicycle.Unicycle(**kwargsUnicycle)
+mvehicle = Unicycle(**kwargsUnicycle)
+
+# World 
+kwargsWorld = {'width': 10, 'height': 10, 'nb_landmarks': 10}
+mworld = LandmarkWorld(**kwargsWorld)
 
 # Sensor
-kwargsLandmark = {'max_range': 5.0}
-msensor = landmark.Landmark(**kwargsLandmark)
-msensor.create_world(world_size=np.array([10,10]), nb_landmarks=10)
+kwargsLandmark = {'world': mworld,'max_range': 5.0}
+msensor = LandmarkSensor(**kwargsLandmark)
+
+# Plot
+kwargsPlot = {'world': mworld}
+mplot = LandmarkLocalization(**kwargsPlot)
 
 # Loop
 while(1):
-    u = np.random.rand(2)
-    x = vehicle.run(dt=0.1,u=u)
+    u = np.random.rand(2,1) + np.array([0,-.4]).reshape(2,1)
+    x = mvehicle.run(dt=0.1,u=u)
     measurements = msensor.sample(x)
-    print(measurements)
+    mplot.update(x,x,measurements)
     #print(x)
-    time.sleep(.2)
+    time.sleep(.05)
 
